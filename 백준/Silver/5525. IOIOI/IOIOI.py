4779c47a7d2f7479e0446cc08301b4d072b18b37
anonypatron <rainerhour0904@gmail.com>
2026-03-22 16:06:10 +0900

    [Silver I] Title: IOIOI, Time: 324 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input()
+
+pattern = "IOI"
+i, cnt, answer = 0, 0, 0
+while i < m - 2:
+    if s[i:i + 3] == pattern:
+        i += 2
+        cnt += 1
+        if cnt >= n:
+            answer += 1
+    else:
+        i += 1
+        cnt = 0
+
+print(answer, end='')
